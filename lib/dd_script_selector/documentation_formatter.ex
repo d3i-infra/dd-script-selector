6313b00433ccab4c295bd750cc17d0999cabf34e
trbKnl <ncdeschipper@gmail.com>
2026-06-03 12:11:54 +0200

    added description to docs

--- lib/dd_script_selector/documentation_formatter.ex
+++ lib/dd_script_selector/documentation_formatter.ex
@@ -19,6 +19,8 @@ defmodule DdScriptSelector.DocumentationFormatter do
     """
     # #{platform_name}
 
+    This document describes the data tables and variables included in the data donation flow for #{platform_name}.
+
     #{table_sections}
     """
     |> String.trim()
